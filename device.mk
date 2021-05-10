69ece64ea1e12db7aef5c06ea6f5dec59d9a2569
danascape <danascape@gmail.com>
2021-05-10 08:20:31 +0200

    billie: Import IDC/keylayout
    * OnePlusN10-user-10-QKQ1.200830.002-2103251959-release-keys
    
    Change-Id: I516795045f7a05e9fb8de91955f097fd2dbca25a

--- device.mk
+++ device.mk
@@ -38,6 +38,15 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.base@1.0
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/fpc1020.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/fpc1020.idc
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/fpc1020.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/fpc1020.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
+
 # Partitions - Dynamic
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
