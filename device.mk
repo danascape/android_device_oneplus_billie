f7be08f2c765d050575bd848c6641e159a15770e
danascape <danascape@gmail.com>
2021-09-01 08:14:14 +0200

    billie: Import relevant Bluetooth overlay
    Change-Id: I06bd155224c4fe325a39dac41686ae8ac1b63fa3

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     OnePlusIconShapeCircleOverlay \
