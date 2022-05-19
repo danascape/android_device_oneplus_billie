80c002155ad2867501df76f64c79f8ba6a4148f1
danascape <danascape@gmail.com>
2022-05-19 03:33:32 -0600

    billie: update more blobs

--- extract-files.sh
+++ extract-files.sh
@@ -73,3 +73,6 @@ function fix_xml_version () {
         's/xml version="2.0"/xml version="1.0"/' \
         "$DEVICE_BLOB_ROOT"/"$1"
 }
+
+fix_xml_version product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml
+fix_xml_version product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml
