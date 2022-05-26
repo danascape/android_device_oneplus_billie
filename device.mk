a15f817a2eb65d192b9b2b0d78e461e1fcdf48b2
danascape <danascape@gmail.com>
2022-05-26 12:52:32 -0600

    billie: disable apex for now

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
