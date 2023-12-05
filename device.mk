286bc59274113b436a8a4eaad8a897dae31f62a0
Saalim Quadri <danascape@gmail.com>
2023-12-05 17:40:25 +0530

    billie: Uprev QTI USB to V1.3
    Change-Id: I0529062973643929d534a4013724c7dce7601bc6
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -604,7 +604,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-qti
+    android.hardware.usb@1.3-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
