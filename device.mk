069c34b6dc2d0fb2e12599407ef47e046df3194b
Saalim Quadri <danascape@gmail.com>
2023-12-05 17:41:21 +0530

    billie: Drop Snapdragon Camera
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Change-Id: Iaadd7cc7c37307947da43ad10f60509f27f54380
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -238,9 +238,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-PRODUCT_PACKAGES += \
-    Snap
-
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
